8b5c34c81c9b0d47556b35c13af2c14bf5ab96d9
Ekim Emre Yardimli <ekimyardimli@izlenim.com>
2025-05-18 20:02:05 +0800

    fav

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -10,6 +10,7 @@
 	<link rel="stylesheet" href="{{ asset('vendors/fontawesome-free-6.7.2/css/all.min.css') }}">
 	<link rel="stylesheet" href="{{ asset('css/admin.css') }}">
 	
+
 	<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/apple-touch-icon.png') }}">
 	<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon-32x32.png') }}">
 	<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon-16x16.png') }}">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,10 +18,10 @@
 	<link href="{{ asset('template/assets/css/responsive.css') }}" rel="stylesheet" />
 	<link href="{{ asset('/css/style.css') }}" rel="stylesheet" />
 	
-	<link rel="apple-touch-icon" sizes="180x180" href="template/assets/img/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="template/assets/img/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="template/assets/img/favicon-16x16.png">
-	<link rel="manifest" href="template/assets/img/site.webmanifest">
+	<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/apple-touch-icon.png') }}">
+	<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon-32x32.png') }}">
+	<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon-16x16.png') }}">
+	<link rel="manifest" href="{{ asset('images/site.webmanifest') }}">
 	
 	<title>@yield('title', 'Free Kindle Covers')</title>
 	
