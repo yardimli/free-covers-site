ca49a7ac0fb813812557f7bb83c6b5d4b01c2aba
Ekim Emre Yardimli <ekimyardimli@izlenim.com>
2025-06-02 17:16:08 +0800

    make top search work

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,9 +2,11 @@
 	<nav class="navbar navbar-expand-lg menu_one menu_white" id="header">
 		<div class="container">
 			<a class="navbar-brand sticky_logo" href="{{ route('home') }}">
-				<img src="{{ asset('template/assets/img/home/logo-dark.png') }}" alt="logo" />
+				<img src="{{ asset('template/assets/img/home/logo-dark.png') }}" alt="logo"/>
 			</a>
-			<button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse"
+			        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+			        aria-label="Toggle navigation">
                 <span class="menu_toggle">
                     <span class="hamburger">
                         <span></span>
@@ -25,12 +27,14 @@
 						</a>
 					</li>
 					<li class="nav-item {{ request()->routeIs('shop.*') ? 'active' : '' }}">
-						<a class="nav-link" href="{{ route('shop.index') }}" role="button" aria-haspopup="true" aria-expanded="false">
+						<a class="nav-link" href="{{ route('shop.index') }}" role="button" aria-haspopup="true"
+						   aria-expanded="false">
 							Browse Covers
 						</a>
 					</li>
 					<li class="nav-item {{ request()->routeIs('blog.*') ? 'active' : '' }}">
-						<a class="nav-link" href="{{ route('blog.index') }}" role="button" aria-haspopup="true" aria-expanded="false">
+						<a class="nav-link" href="{{ route('blog.index') }}" role="button" aria-haspopup="true"
+						   aria-expanded="false">
 							Blog
 						</a>
 					</li>
@@ -43,10 +47,14 @@
 				<div class="alter_nav">
 					<ul class="navbar-nav search_cart menu">
 						<li class="nav-item search">
-							<a class="nav-link search-btn" href="javascript:void(0);"><i class="icon-search"></i></a>
-							<form action="#" method="get" class="menu-search-form">
+							<a class="nav-link search-btn" href="javascript:void(0);" aria-label="Open search"><i
+									class="icon-search"></i></a>
+							<form action="{{ route('shop.index') }}" method="GET" class="menu-search-form">
 								<div class="input-group">
-									<input type="search" class="form-control" placeholder="Search here.." />
+									<input type="hidden" name="orderby" value="latest">
+									<input type="hidden" name="category" value="">
+									<input type="search" name="s" class="form-control" placeholder="Search covers..."
+									       aria-label="Search covers"/>
 									<button type="submit">
 										<i class="ti-arrow-right"></i>
 									</button>
@@ -62,7 +70,7 @@
 						</li>
 						@guest
 							<li class="nav-item user ms-3">
-							<a class="nav-link" href="{{ route('register') }}"><i class="icon_id-2_alt"></i></a>
+								<a class="nav-link" href="{{ route('register') }}"><i class="icon_id-2_alt"></i></a>
 							</li>
 						@endguest
 					</ul>
