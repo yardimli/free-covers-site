740f2afdaabd306c919b52aaec0d82de6fed0fc3
Ekim Emre Yardimli <ekimyardimli@izlenim.com>
2025-06-10 18:30:21 +0800

    Update ShopController.php

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -1,7 +1,6 @@
 <?php namespace App\Http\Controllers;
 
 use App\Models\Cover;
-use Artesaos\SEOTools\SEOTools;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Artesaos\SEOTools\Facades\SEOTools;
